c8d4623eb9f3a41d7ca365b246a83dd2e42082e4
Robert Pincus <Robert.Pincus@colorado.edu>
2017-06-21 11:14:50 -0400

    Initial readme; script to generate new templates per https://docs.google.com/document/d/1h0r8RZr_f3-8egBMMh7aqLwy3snpD6_MrDz1q8n5XUk/edit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RFMIP-IRF-Scripts
 Scripts useful for offline radiative transfer calculations for Radiative Forcing Model Intercomparison Project
+
+For the moment this is simply a Python script creating ESGF- and CMIP6-compliant files for offline 
+radiative transfer calculations. The script requires the netCDF4 module. 
--- generate-output-file-templates.py
+++ generate-output-file-templates.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
-# Prepare files containing optimized subsets of columns for distribution on the 
-#   Earth System Grid 
+# Prepare templates for output files from offline radiative transfer calculations 
+#   suitable for publishing on the Earth System Grid 
 #
 # Robert Pincus, Robert.Pincus@colorado.edu, 2016-2017
 #
@@ -19,37 +19,52 @@ def copyVar(nc_in, nc_out, name, newname=None) :
 # ---------------------------------------------------------------------------------
 
 atmos_file = Dataset('multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-0-4_none.nc', mode='r') 
+# Available from https://www.earthsystemcog.org/projects/rfmip/resources/
 
 short_names = ['rlu','rsu', 'rld', 'rsd'] 
 stand_names = ['upwelling_longwave_flux_in_air','upwelling_shortwave_flux_in_air', 
                'downwelling_longwave_flux_in_air','downwelling_shortwave_flux_in_air'] 
 
-               
+# Attributes are take from https://docs.google.com/document/d/1h0r8RZr_f3-8egBMMh7aqLwy3snpD6_MrDz1q8n5XUk/edit
+# Data reference syntax attributes 
+drs_attrs = { 
+  "activity_id"  :"RFMIP", #  (from CMIP6_activity_id.json)
+  "product"      :"model_output",
+  "experiment_id": "rad-irf", #(from CMIP6_experiment_id.json)
+  "table_id"     :"Efx", # (per http://clipc-services.ceda.ac.uk/dreq/u/efc0de22-5629-11e6-9079-ac72891c3257.html) 
+  "frequency"    :"fx", 
+  "sub_experiment_id":"none"} 
+
+# Further required attributes, uniform across submissions 
+std_attrs = { 
+  "Conventions":"CF-1.7 CMIP-6.0", 
+  "data_specs_version":"1.00.12", 
+  "experiment":"rad_irf", 
+  "sub_experiment":"none", 
+  "forcing_index":1, 
+  "physics_index":1} 
+
+# Model/institution specific attributes 
+model_attrs = {
+  "institution_id"  :"",
+  "source_id"       :"",
+  "further_info_url":"",
+  "license"         :""} 
+
+# Submission attrs 
+sub_attrs = {
+  "creation_date":"",
+  "variant_label":"", 
+  "version"      :""} 
+
 for short, std in zip(short_names, stand_names) : 
     out_file_name = short + "_template.nc"
     print('Creating ' + out_file_name)
     out_file = Dataset(out_file_name, mode='w', FORMAT='NETCDF4_CLASSIC')
-    out_file.setncatts({
-       'title':'',
-       'institution_id':'', 
-       'institution'   :'', 
-       'activity_id'   :'CMIP', 
-       'Conventions'   :'CF-1.6',  
-       'creation_date' :'', 
-       'realm'         :'atmos', 
-       'data_structure':'sites',  
-       'source'        :'', 
-       'source_id'     :'',
-       'history'       :'',
-       'contact'       :'', 
-       'references'    :'', 
-       'dataset_category'      :'radiation',
-       'dataset_version_number':'',
-       'further_info_url'      :'',
-       'mip_era'       :'CMIP6',
-       'target_mip'    :'RFMIP',
-       'grid_label'    :'none',
-       'license':''})
+    out_file.setncatts(drs_attrs) 
+    out_file.setncatts(std_attrs) 
+    out_file.setncatts(model_attrs) 
+    out_file.setncatts(sub_attrs) 
     d = out_file.createDimension('expt',  atmos_file.dimensions['expt'].size) 
     d = out_file.createDimension('site',  atmos_file.dimensions['site'].size) 
     d = out_file.createDimension('level', atmos_file.dimensions['level'].size) 
@@ -58,7 +73,8 @@ for short, std in zip(short_names, stand_names) :
     copyVar(atmos_file, out_file, 'time') 
     copyVar(atmos_file, out_file, 'pres_level', 'plev') 
     v = out_file.createVariable(short,   'f4', ('expt', 'site', 'level')) 
-    v.setncatts({'standard_name':std, 
+    v.setncatts({'variable_id'  :short,
+                 'standard_name':std, 
                  'units'        :'W m-2', 
                  'coordinates'  : 'lon lat time'}) 
     copyVar(atmos_file, out_file, 'profile_weight') 

eb9e33514fba24328952b98f9be44181387e4b27
Robert Pincus <Robert.Pincus@colorado.edu>
2019-02-06 15:13:59 -0500

    License attribute is working

--- generate-output-file-templates.py
+++ generate-output-file-templates.py
@@ -23,23 +23,17 @@ atmos_file = Dataset('multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-1-1_no
 # Available from https://www.earthsystemcog.org/projects/rfmip/resources/
 # or from https://esgf-node.llnl.gov/search/input4mips/ ; search for "RFMIP"
 
-short_names = ['rlu','rsu', 'rld', 'rsd']
-stand_names = ['upwelling_longwave_flux_in_air','upwelling_shortwave_flux_in_air',
-               'downwelling_longwave_flux_in_air','downwelling_shortwave_flux_in_air']
-
 #
-# Required attributes, uniform across submissions
-#
-std_attrs = {
-  "data_specs_version":"01.00.12",
-  "physics_index":1}
-
-#
-# Model/institution specific attributes
+# Model/institution specific attributes - replace these with values suitable for your institution, model, etc. 
 #
 institution_id = "AER"
 source_id      = "LBLRTM-12-8"
-variant_label  = "r1i1p1f1"
+physics_index = 1 # Use, e.g. for different approximations
+forcing_index = 1  # This values follows page 2074 in https://dx.doi.org/10.5194/gmd-10-2057-2017
+                   # 1 = calculations uses all available greenhouse gases
+                   # 2 = calculation uses CO2, CH4, N2O, CFC11eq
+                   # 3 = calculation uses CO2, CH4, N2O, CFC12eq, HFC-134eq
+variant_label  = "r1i1p{0}f{1}".format(physics_index,forcing_index)
 model_attrs = {
   "institution_id"  :"AER",
   "institution"     :"Research and Climate Group, Atmospheric and Environmental Research, 131 Hartwell Avenue, Lexington, MA 02421, USA",
@@ -48,15 +42,19 @@ model_attrs = {
   "source"          :"LBLRTM 12.8 (2017): \naerosol: none\natmos: none\natmosChem: none\nland: none\nlandIce: none\nocean: none\nocnBgchem: none\nseaIce: none",
                      # Needs to match entry for source_id in CMIP6 controlled vocabulary
   "further_info_url":"https://furtherinfo.es-doc.org/CMIP6." + institution_id + "." + source_id + ".rad-irf.none." + variant_label,
-  "forcing_index"   :1, # This values follows page 2074 in https://dx.doi.org/10.5194/gmd-10-2057-2017
-                     # 1 = calculations uses all available greenhouse gases
-                     # 2 = calculation uses CO2, CH4, N2O, CFC11eq
-                     # 3 = calculation uses CO2, CH4, N2O, CFC12eq, HFC-134eq
-  "license"         :'CMIP6 model data produced by AER is licensed under a Creative Commons Attribution "Share Alike 4.0" International License (https://creativecommons.org/licenses). ' +
-                      "Consult https://pcmdi.llnl.gov/CMIP6/TermsOfUse for terms of use governing CMIP6 output, including citation requirements and proper acknowledgment. " +
-                      "Further information about this data, including some limitations, can be found via the further_info_url (recorded as a global attribute in this file). " +
-                      "The data producers and data providers make no warranty, either express or implied, including, but not limited to, warranties of merchantability and fitness for a particular purpose. " +
-                      "All liabilities arising from the supply of the information (including any liability arising in negligence) are excluded to the fullest extent permitted by law." }
+  "forcing_index"   :int(forcing_index),
+  "license"         :"CMIP6 model data produced by " + institution_id + " is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License (https://creativecommons.org/licenses). " +
+                     "Consult https://pcmdi.llnl.gov/CMIP6/TermsOfUse for terms of use governing CMIP6 output, including citation requirements and proper acknowledgment. " +
+                     "Further information about this data, including some limitations, can be found via the further_info_url (recorded as a global attribute in this file) and at https://pcmdi.llnl.gov/." +
+                     "The data producers and data providers make no warranty, either express or implied, including, but not limited to, warranties of merchantability and fitness for a particular purpose." +
+                     "All liabilities arising from the supply of the information (including any liability arising in negligence) are excluded to the fullest extent permitted by law." }
+
+#
+# Required attributes, uniform across submissions
+#
+std_attrs = {
+  "data_specs_version":"01.00.29",
+  "physics_index":int(physics_index)}
 
 # Submission attrs
 sub_attrs = {
@@ -75,19 +73,22 @@ drs_attrs = {
   "sub_experiment_id":"none"}
 
 expt_attrs = {
-  "Conventions"         :"CF-1.7 CMIP-6.0",
+  "Conventions"         :"CF-1.7 CMIP-6.2",
   "mip_era"             :"CMIP6",
   "experiment"          :"rad_irf",
   "sub_experiment"      :"none",
   "product"             :"model-output",
-  "realization_index"   :1,
-  "initialization_index":1,
+  "realization_index"   :int(1),
+  "initialization_index":int(1),
   "source_type"         :"RAD",
   "nominal_resolution"  :"10 km",
   "realm"               :"atmos",
   "grid_label"          :"gn",
   "grid"                :"columns sampled from ERA-Interim, radiative fluxes computed independently"}
 
+short_names = ['rlu','rsu', 'rld', 'rsd']
+stand_names = ['upwelling_longwave_flux_in_air','upwelling_shortwave_flux_in_air',
+               'downwelling_longwave_flux_in_air','downwelling_shortwave_flux_in_air']
 
 for short, std in zip(short_names, stand_names) :
     out_file_name = short + ".nc"
